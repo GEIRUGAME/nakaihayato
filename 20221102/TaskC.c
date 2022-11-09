50b251ef113e1a62b90be29e207b7136f662662e
MSI\kuron <kuronosu_game@icloud.com>
2022-11-09 16:32:32 +0900

    TaskC

--- 20221102/TaskC.c
+++ 20221102/TaskC.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-int main() {
+int main(void) {
 
-    srand ( time ( NULL ) );
-
-    int i = rand() % 10 + 1;
-    
-    printf ( "%d\n", i );
+    int data[10]={1,2,3,4,5,6,7,8,9,10};
+    for(int i=0;i<=10;i++){
+        data[i]=rand();
+    }
+    for(int i=1;i<=10;i++){
+        printf("%d\n",data[i]);
+    }
 
     return 0;
 }
