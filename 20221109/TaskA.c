96c549b1115c3b6440e9c95b6493b8cbd82167f1
MSI\kuron <kuronosu_game@icloud.com>
2022-11-16 17:13:48 +0900

    TaskA

--- 20221109/TaskA.c
+++ 20221109/TaskA.c
@@ -1,21 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-void shuffle(int array[],int size);
+void shuffle(int array[], int size);
 
-int main(){
-    int data[11] = {0,1,2,3,4,5,6,7,8,9,10};
-    int size = sizeof(data);
+int main() {
+    int data[11] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    // 今の配列がどうなってるかを出力 = 初期化できているかどうか
+
+    int all = sizeof(data); // 全体のバイト数 sizeof(data)
+    int one = sizeof(data[0]); //　1つのバイト数 sizeof(data[0])
+    int size = all / one ; // data1の要素数 size
+    // sizeを出力 = 処理に必要な値が計算できているか
+    printf("size:%d\n", size);
+
     int result = -1;
     int target = 5;
     
     shuffle(data, size);
+    // 今の配列がどうなっているかを出力 = シャッフルできているかどうか
+
     for (int i = 0; i < size; ++i) {
         if(data[i] == target){
             result = i;
+            break;
         }
     }
-    printf("targetのインデックスはresultです%d\n",result );
+
+    // 結果を出力
+    printf("target noindekkusuha %d desu\n", result);
     return 0;
 }
 
