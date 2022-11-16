d0e8b6405fd707f303e405a4f931f0419d0d7330
MSI\kuron <kuronosu_game@icloud.com>
2022-11-16 14:43:50 +0900

    TaskC_clear

--- 20221102/TaskC.c
+++ 20221102/TaskC.c
@@ -4,11 +4,9 @@
 
 int main(void) {
 
-    int data[10]={1,2,3,4,5,6,7,8,9,10};
-    for(int i=0;i<=10;i++){
-        data[i]=rand();
-    }
+    int data[10];
     for(int i=1;i<=10;i++){
+        data[i]=rand();
         printf("%d\n",data[i]);
     }
 
