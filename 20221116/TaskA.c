36d71b8653504155cb8df70a9529df4f3cef6805
MSI\kuron <kuronosu_game@icloud.com>
2022-12-14 16:24:29 +0900

    TaskA

---
+++ 20221116/TaskA.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int size = 5;
+    int data[size];
+    int pop;
+    data[size]={1,3,5,7,9};
+    for(int i = 0; i <= size; ++i){
+        printf("data:%d\n", data[i]);
+    }
+    return 0;
+}
+void pop(){
+
+}
