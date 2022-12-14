dd6902720c21e6e3f3a06317abbbcdcd684ee667
MSI\kuron <kuronosu_game@icloud.com>
2022-12-14 15:42:08 +0900

    TaskCクリア

--- 20221109/TaskC.c
+++ 20221109/TaskC.c
@@ -3,7 +3,53 @@
 int main(){
     int size = 100;
     int data[size];
+    int valueA;
+    int valueB;
+    int max;
+    int min;
+    int target = 50;
+    int result = -1;
+    int left = 0;
+    int right = data[size] - 1;
+    int mid;
+
     for(int i = 0; i < size; i++) {    
-        data[i] = rand() ;
+        data[i] = rand() % 100 + 1;
+    }
+
+    for(int i = 0; i < size; ++i){
+        for(int j = size - 1; j >= i + 1; --j){
+            valueA = data[i];
+            valueB = data[j];
+            if(valueA > valueB){
+                max = valueA;
+                min = valueB;
+                data[i] = min;
+                data[j] = max;
+            }
+        }
+    }
+
+    for(left = 0;left < right;left++){
+        mid = (left + right) / 2;
+        if(data[mid]==target){
+            result = mid;
+            break;
+        }
+        else if(data[mid] < target){
+            left = mid + 1;
+        }
+        else{
+            right = mid;
+        }        
+    }
+    for(int i = 0; i <= size; ++i){
+        printf("data:%d\n", data[i]);
+    }
+    if(result >= 0){
+        printf("target no indekkusu ha %d desu\n", result);
+    }
+    else{
+        printf("target ha mitukarimasenndesita\n");
     }
 }
