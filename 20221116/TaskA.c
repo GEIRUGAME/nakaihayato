8d1677125d776a25557f90a0e303bcf928282e52
MSI\kuron <kuronosu_game@icloud.com>
2022-12-21 16:24:34 +0900

    TaskAまだまだ

--- 20221116/TaskA.c
+++ 20221116/TaskA.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+int Stack[];
 int main(){
     int size = 5;
     int data[size];
     int pop;
-    data[size]={1,3,5,7,9};
-    for(int i = 0; i <= size; ++i){
+    int DLdate = 0;
+    for(int i = 1; i <= 5; ++i){
+        data[i] = DLdate + i;
+        DLdate += 1;
         printf("data:%d\n", data[i]);
     }
     return 0;
+}
+
+void Push(){
+
 }
 void pop(){
 
